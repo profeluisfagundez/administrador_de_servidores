6612d6efaacd6abfe4249a9a37071a3f6768e6bf
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2022-06-29 20:34:33 -0300

    Se creo una función con el menu principal

--- main.sh
+++ main.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #SCRIPT INICIAL DEL SERVIDOR
+
+function menu(){
+    echo "MENÚ PRINCIPAL"
+    echo "1 - Menú de gestión de usuarios"
+    echo "2 - Menú de gestión de grupos"
+    echo "3 - Menu de gestión de respaldos"
+    echo "4 - Menú de gestión de redes"
+    echo "0 - Salir"
+}
